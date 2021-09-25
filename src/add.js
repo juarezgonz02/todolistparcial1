98045209bd02579cd6cf2aa1c5aef4c5ed073514
Oscar Juarez <00126320@uca.edu.sv>
2021-09-24 19:42:34 -0600

    Final commit fixed bugs in list deletion

--- src/add.js
+++ src/add.js
@@ -1,11 +1,11 @@
 const addTask = () => {
     let taskDescription = taskDescriptionInput.value
-    
-    let newTask = { 
+
+    let newTask = {
         id: taskList.length + 1,
         description: taskDescription,
-        completed: false 
-    } 
+        completed: false
+    }
 
     taskList.push(newTask);
     taskDescriptionInput.value = "";
@@ -17,65 +17,69 @@ const addTask = () => {
 const iconCompleteClass = "fas fa-check";
 const iconDeleteClass = "fas fa-trash";
 
-const createTask = ({id, description, completed}) => {
+const createTask = ({ id, description, completed }) => {
 
     ///////////////////////////////
     //// HTML ELEMENTS CREATE ////
     let newTaskContainer = document.createElement("div")
-    let descriptionContainer = document.createElement("div") 
+    let descriptionContainer = document.createElement("div")
     let buttonsContainer = document.createElement("div")
     let iconComplete = document.createElement("i")
-    let iconDelete =  document.createElement("i")
+    let iconDelete = document.createElement("i")
     let taskDescription = document.createElement("textarea")
     ////////////////////////////////
     newTaskContainer.className = "task"
+    taskDescription.className = "description"
     descriptionContainer.className = "description-container"
     buttonsContainer.className = "buttons-container"
     iconComplete.className = iconCompleteClass
     iconDelete.className = iconDeleteClass;
-    
-    
-    if(completed)
-        taskDescription.className = "description completed"
-    else
-        taskDescription.className = "description"
-    
+
+
+    if (completed)
+        newTaskContainer.className = "task completed"
+
+    /**
+     * HERE, TASK TEXT IS ADDED AND CHANGED
+     */
     taskDescription.name = `${id}`;
     taskDescription.innerText = description
-    
+
     taskDescription.addEventListener("change", (e) => {
-        taskList[e.target.name].description = e.target.value 
+        taskList[e.currentTarget.name].description = e.currentTarget.value
         showTasks();
     })
 
     descriptionContainer.appendChild(taskDescription)
 
-    /*  COMPLETION AND DELETION BUTTONS AND ITS EVENTS */ 
+    /*  COMPLETION AND DELETION BUTTONS AND ITS EVENTS */
     let completeButton = document.createElement("button");
     completeButton.className = "complete-task";
-    completeButton.value = `${id}`;
+    completeButton.name = `${id}`;
     completeButton.id = "complete-task";
-    iconComplete.value = `${id}`
     completeButton.addEventListener("click", (e) => {
-        
-        
-        if (taskList[e.target.value].completed == true)
-            taskList[e.target.value].completed = false;
-        else
-            taskList[e.target.value].completed = true;
 
+
+
+        if (taskList[e.currentTarget.name].completed == true)
+            taskList[e.currentTarget.name].completed = false;
+        else
+            taskList[e.currentTarget.name].completed = true;
+        debugger;
         showTasks();
-        
+
     })
     completeButton.appendChild(iconComplete);
+
+
     //------------
     let deleteButton = document.createElement("button");
     deleteButton.className = "delete-task";
-    deleteButton.value = `${id}`;
-    deleteButton.id = "delete-task";    
+    deleteButton.name = `${id}`;
+    deleteButton.id = "delete-task";
 
     deleteButton.addEventListener("click", (e) => {
-        taskList.splice(parseInt(e.target.value), 1) 
+        taskList.splice(parseInt(e.currentTarget.name), 1)
         showTasks();
     })
 
@@ -90,7 +94,7 @@ const createTask = ({id, description, completed}) => {
 
 
     return newTaskContainer;
-} 
+}
 
 addTaskButton.addEventListener("click", addTask)
 
--- src/index.js
+++ src/index.js
@@ -3,45 +3,44 @@ const taskDescriptionInput = document.getElementById("task-description")
 const tasksContainer = document.getElementById("tasks");
 const storage = window.localStorage;
 
-if(localStorage.getItem("tasks") != null)
+if (localStorage.getItem("tasks") != null)
     var taskList = JSON.parse(localStorage.getItem("tasks"))
-else 
+else
     var taskList = [];
 
 
-taskDescriptionInput.addEventListener("input", (e)=>{
+taskDescriptionInput.addEventListener("input", (e) => {
 
-     if(e.target.value.length > 0){
+    if (e.target.value.length > 0) {
         addTaskButton.disabled = false;
     }
-    else{
+    else {
         addTaskButton.disabled = true;
     }
 })
 
-taskDescriptionInput.addEventListener("keypress", (e)=>{
+taskDescriptionInput.addEventListener("keypress", (e) => {
 
-     if(e.target.value.length > 0 && e.key == "Enter")
+    if (e.target.value.length > 0 && e.key == "Enter")
         addTask();
 })
 
 const showTasks = () => {
     tasksContainer.innerHTML = "";
-    
+
     let i = 0;
-    
-    taskList = taskList.sort((a,b)=>{
-        if (a.completed){
+
+    taskList = taskList.sort((a, b) => {
+        if (a.completed) {
             return 1
         }
-        else if(b.completed){
+        else if (b.completed) {
             return -1
         }
-        else if(!b.completed && !a.completed){
+        else if (!b.completed && !a.completed) {
             return 0;
         }
-        else 
-        {
+        else {
             return 1;
         }
     })
@@ -50,9 +49,9 @@ const showTasks = () => {
     taskList.forEach(task => {
         task.id = i
         tasksContainer.appendChild(createTask(task))
-        i+=1
+        i += 1
     })
-    
+
     localStorage.setItem("tasks", JSON.stringify(taskList))
-    
+
 }
